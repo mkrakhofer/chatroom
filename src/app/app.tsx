b297f6abfd35a79e76091ffc57f4994300711798
Michael Krakhofer <michael.krakhofer@gmail.com>
2019-04-13 20:02:32 +0200

    spelling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Hello } from './components/Hello';
+import { Hello } from './components/hello';
 declare let module: any
 
 ReactDOM.render(<Hello compiler="Typescript" framework="React" bundler="Webpack" />,

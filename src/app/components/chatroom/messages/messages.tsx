8316f1e89729dcc6cd76e880ddc80a540d223dcc
Michael Krakhofer <michael.krakhofer@gmail.com>
2019-04-28 10:36:00 +0200

    autoscroll if user scrolls to botom

--- src/app/components/chatroom/messages/messages.tsx
+++ src/app/components/chatroom/messages/messages.tsx
@@ -7,8 +7,31 @@ import { observer } from "mobx-react-lite";
 export const Messages = observer(() => {
 
     const messageStore = React.useContext(MessageStoreContext);
+    const messages = React.useRef<HTMLDivElement>(null);
+    const [autoScroll, setAutoScroll] = React.useState(true);
 
-    return <div className="messages">
+    const handleScroll = () => {
+        const scrollTop = messages.current.scrollTop;
+        const scrollHeight = messages.current.scrollHeight;
+        const clientHeight = messages.current.clientHeight;
+        if(scrollHeight - scrollTop - clientHeight < 10) {
+            setAutoScroll(true);
+        }
+        else {
+            setAutoScroll(false);
+        }
+    }
+
+    React.useEffect(() => {
+        if(autoScroll) {
+            const scrollHeight = messages.current.scrollHeight;
+            const height = messages.current.clientHeight;
+            const maxScrollTop = scrollHeight - height;
+            messages.current.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0; 
+        }
+    });
+
+    return <div onScroll={handleScroll} ref={messages} className="messages">
         {messageStore.messages.map(m => <Message message={m}/>)}
     </div>;
 });
